0b98a6ebecc02103b926a4b3b0d09eb44c7382c0
bookendus <bookendus@gmail.com>
2021-09-18 20:42:46 +0900

    창 띄우기 성공

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
-using namespace std;
+// using namespace std;
 
 int main(int argc, char** argv) {
-    SPDLOG_INFO("Hello, world!");
+    SPDLOG_INFO("Initialize glfw");
+    if (!glfwInit()) {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
+
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window) {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    // glfw 루프 실행, 윈도우 close 버튼을 누르면 ㅈㅇ상 종료
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+    glfwTerminate();
+
     return 0;
 }
 
